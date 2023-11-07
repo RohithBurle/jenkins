4787d69f2d8bdda98d5e4c1d81165440676065cf
RohithBurle <144802331+RohithBurle@users.noreply.github.com>
2023-11-08 00:13:29 +0530

    added factorial code

--- java.java
+++ java.java
@@ -1,5 +1,15 @@
 public class java {
-  public static void main(String[] args) {
-    System.out.println("Hello World");
-  }
+    public static void main(String[] args) {
+        int number = 5; // Change this number to calculate the factorial for a different number
+        long factorial = factorial(number);
+        System.out.println("Factorial of " + number + " = " + factorial);
+    }
+
+    public static long factorial(int n) {
+        if (n == 0) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
 }
